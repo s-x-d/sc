d88bfa0bb36da5baa71e5ad581a3d869a9f54dad
s-x-d <robin@systemxdesign.com>
2025-06-06 10:34:58 +0100

    settings localhost update

--- aws4/settings.py
+++ aws4/settings.py
@@ -78,7 +78,8 @@ if database_url:
                 'NAME': os.environ.get('DB_NAME', 'postgres'),
                 'USER': os.environ.get('DB_USER', 'postgres'),
                 'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-                'HOST': os.environ.get('DB_HOST', 'localhost'),
+                'HOST': os.environ.get('DB_HOST'),
+                #'HOST': os.environ.get('DB_HOST', 'localhost'),
                 'PORT': os.environ.get('DB_PORT', '5432'),
             }
         }
