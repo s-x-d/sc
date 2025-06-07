0695e9686fd0fa4cf572b844120c4d660ed10386
s-x-d <robin@systemxdesign.com>
2025-06-07 14:54:40 +0100

    settings and config

--- aws4/settings.py
+++ aws4/settings.py
@@ -78,8 +78,7 @@ if database_url:
                 'NAME': os.environ.get('DB_NAME', 'postgres'),
                 'USER': os.environ.get('DB_USER', 'postgres'),
                 'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-                'HOST': os.environ.get('DB_HOST'),
-                #'HOST': os.environ.get('DB_HOST', 'localhost'),
+                'HOST': os.environ.get('DB_HOST', 'localhost'),
                 'PORT': os.environ.get('DB_PORT', '5432'),
             }
         }
@@ -91,8 +90,7 @@ else:
             'NAME': os.environ.get('DB_NAME', 'postgres'),
             'USER': os.environ.get('DB_USER', 'postgres'),
             'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-            'HOST': os.environ.get('DB_HOST'),
-            #'HOST': os.environ.get('DB_HOST', 'localhost'),
+            'HOST': os.environ.get('DB_HOST', 'localhost'),
             'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }
